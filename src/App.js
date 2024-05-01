e8baabd2c7e3dccb9f0cd458524c4e7f7ee768c4
Ji-Sung05 <dlwltjd6847@gmail.com>
2024-05-01 18:56:51 +0900

    주석 생성

--- src/App.js
+++ src/App.js
@@ -10,7 +10,8 @@ function App() {
         'Authorization': `Bearer ${process.env.REACT_APP_API_TOKEN}` //만료일 24년 5월 8일
       }
     };
-    const { data } = await axios.get('https://api.github.com/repos/Ji-Sung05/loginform/commits', config);
+    const { data } = await axios.get('https://api.github.com/repos/Ji-Sung05/GIT_API/commits', config);
+    //const { data } = await axios.get('https://api.github.com/repos/Chat-ITC/WITH_Client/commits', config);
     setTodos(data);
   } catch (error) {
     console.error(error);
@@ -27,7 +28,7 @@ function App() {
       {todos ? (
         <ul>
           {todos.map((commit, index) => (
-            <li key={index}><a href={commit.html_url} aria-label="Link">{commit.sha}</a></li>
+            <li key={index}><a href={commit.html_url} aria-label="Link">{commit.sha} {commit.commit.message}</a></li>
           ))}
         </ul>
       ) : (
